6a4eae20334ad2b7acab02538303879dffc9b081
monju1125 <monju1125@github.com>
2026-06-28 05:56:12 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-06-28T05:56:11.979Z

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -16,11 +16,10 @@ public class InsertPosition {
      */
     public int searchInsert(int[] nums, int target) {
 
-        for(int i =0; i < nums.length ; i++){
+        for(int i =0; i < nums.length -1; i++){
             if ( target > nums[i] &&  target < nums[i+1]  ) {
                 return i;
             }
-            else continue;
         }
 
         return -1;
